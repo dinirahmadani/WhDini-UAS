1efba81473ee887443d5bf232b80ac3c96ef9f91
Dini Rahmadani <rahmadanidini68@gmail.com>
2025-05-31 10:58:04 +0700

    uas-selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,5 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Dini";
+  static String gambar =
+      "assets/mahasiswa/dini.jpeg";
 }
